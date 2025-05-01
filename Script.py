08e1bc7fbdf9070cc56482d97ea7ccfc60a97ab0
rohitreddy404 <antmangktifjfjtjfjft@gmail.com>
2025-05-01 18:33:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -11,7 +11,7 @@ class script(object):
 # Ask Doubt on telegram @KingVJ01
 
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
+    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : @animegalaxy4u {file_name}
 
 ⚙️ sɪᴢᴇ : {file_size}
 
